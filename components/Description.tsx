d3433e074065ab444f39724878d5e2f3f51eab73
FrozenKota <q38230kota@gmail.com>
2022-08-29 18:51:11 +0900

    使い方 画像サイズ調整完了

--- components/Description.tsx
+++ components/Description.tsx
@@ -6,11 +6,10 @@ const {width, height} = Dimensions.get('window');
 const Description = (props: any) => {
     const {closeHandler} = props;
 
-
     const CloseButton = () => {
         return (
-            <TouchableOpacity>
-
+            <TouchableOpacity style={styles.closeBtn} onPress={()=>{closeHandler()}}>
+                <Text style={{fontSize: width*0.06, color: 'white'}}>✕</Text>
             </TouchableOpacity>
         )
     }
@@ -19,33 +18,47 @@ const Description = (props: any) => {
         <View style={styles.mainContainer}>
             <View style={styles.mainLayout} >
             {/* copy and paste from here ****************************** */}
-            <ScrollView style={{flex:1, flexDirection: 'column'}}> 
-                <Text style={styles.h1} >使　用　方　法</Text>
-                <Text style={styles.h2} >１．セーブデータの作成</Text>
-                <Image style={styles.img1} source={require('../Asset/HowToUse/1_0_createData.jpg')} resizeMode='contain' />
-                <Text style={styles.h3} >「はじめる」をタップします</Text>
 
-                <Image style={styles.img1} source={require('../Asset/HowToUse/1_1_inputFileName.jpg')} resizeMode='contain' />
-                <Text style={styles.h3} >ファイル名を入力し、「決定」をタップします</Text>
-
+            <ScrollView> 
+                <Text style={styles.h1} >使 い 方</ Text>
+                <Text> </Text>
+                <Text style={styles.h2} >１．セーブデータの作成</ Text>
+                <Image style={{width: width,height:367*(width/500)}} source={require('../Asset/HowToUse/1_0_createData.jpg')} resizeMode='contain' />
+                <Text style={styles.h3} >「はじめる」をタップします</ Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Image style={{width: width,height:399*(width/500)}} source={require('../Asset/HowToUse/1_1_inputFileName.jpg')} resizeMode='contain' />
+                <Text style={styles.h3} >ファイル名を入力し、「決定」をタップします</ Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
                 <Text style={styles.h2} >２．場所を選択</ Text>
-                <Image style={styles.img1} source={require('../Asset/HowToUse/2_1_selectPlace.jpg')} resizeMode='contain' />
-                <Text style={styles.h3} >地図を動かし、場所を選択します</Text>
-
-                <Image style={styles.img1} source={require('../Asset/HowToUse/2_2_selectPlace.jpg')} resizeMode='contain' />
+                <Image style={{width: width,height:847*(width/500)}} source={require('../Asset/HowToUse/2_1_selectPlace.jpg')} resizeMode='contain' />
+                <Text style={styles.h3} >地図を動かし、場所を選択します</ Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Image style={{width: width,height:261*(width/500)}} source={require('../Asset/HowToUse/2_2_selectPlace.jpg')} resizeMode='contain' />
                 <Text style={styles.h3} >「決定」をタップします</ Text>
-                <Text></Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
                 <Text style={styles.h2} >３．下地レイヤの編集</ Text>
-                <Image style={styles.img1} source={require('../Asset/HowToUse/3_1_editBottomLayer.jpg')} resizeMode='contain' />
+                <Image style={{width: width,height:479*(width/500)}} source={require('../Asset/HowToUse/3_1_editBottomLayer.jpg')} resizeMode='contain' />
                 <Text style={styles.h3} >土、海などの下地レイヤを編集します。</ Text>
                 <Text style={styles.h3} >「メニュー」をタップしサイドバーを開きます</ Text>
                 <Text style={styles.h3} >次に、「下地レイヤ編集」を有効にします</ Text>
-                <Text></Text>
-                <Image style={styles.img1} source={require('../Asset/HowToUse/3_2_choseImage.jpg')} resizeMode='contain' />
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Image style={{width: width,height:800*(width/500)}} source={require('../Asset/HowToUse/3_2_choseImage.jpg')} resizeMode='contain' />
                 <Text style={styles.h3} >「素材」を選択すると素材一覧が開きます</ Text>
                 <Text style={styles.h3} >好きな素材を選択します</ Text>
-                <Text></Text>
-                <Image style={styles.img1} source={require('../Asset/HowToUse/3_3_placeMaterial.jpg')} resizeMode='contain' />
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Image style={{width: width,height:829*(width/500)}} source={require('../Asset/HowToUse/3_3_placeMaterial.jpg')} resizeMode='contain' />
                 <Text style={styles.h3} >画面下に並ぶボタンを使用して画像を配置します</ Text>
                 <Text style={styles.h3} >「＋」・・・画像を追加</ Text>
                 <Text style={styles.h3} >「ー」・・・画像を削除</ Text>
@@ -54,30 +67,45 @@ const Description = (props: any) => {
                 <Text style={styles.h3} >「→」・・・右へ移動</ Text>
                 <Text style={styles.h3} >「↓」・・・下へ移動</ Text>
                 <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
                 <Text style={styles.h2} >４．上地レイヤの編集</ Text>
-                <Image style={styles.img1} source={require('../Asset/HowToUse/4_1_editTopLayer.jpg')} resizeMode='contain' />
+                <Image style={{width: width,height:268*(width/500)}} source={require('../Asset/HowToUse/4_1_editTopLayer.jpg')} resizeMode='contain' />
                 <Text style={styles.h3} >「下地レイヤ編集」を無効にします</ Text>
                 <Text style={styles.h3} >下地レイヤと同様に素材を選択して配置します</ Text>
-                <Text></Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Image style={{width: width,height:709*(width/500)}} source={require('../Asset/HowToUse/4_2_editTopLayer.jpg')} resizeMode='contain' />
                 <Text style={styles.h3} >このように、下地レイヤに重ねて配置できます</ Text>
                 <Text> </Text>
-                <Text style={styles.h2}>５．データの確認</Text>
-                <Image style={styles.img1} source={require('../Asset/HowToUse/5_1_checkWork.jpg')} resizeMode='contain' />
+                <Text> </Text>
+                <Text> </Text>
+                <Text style={styles.h2} >５．データの確認</ Text>
+                <Image style={{width: width,height:226*(width/500)}} source={require('../Asset/HowToUse/5_1_checkWork.jpg')} resizeMode='contain' />
                 <Text style={styles.h3} >グリッド線を非表示にして作品を確認しましょう</ Text>
                 <Text style={styles.h3} >「メニュー」から「グリッド表示」を無効にします。</ Text>
-                <Text></Text>
-                <Text style={styles.h2}>６．スクリーンショット</ Text>
-                <Image style={styles.img1} source={require('../Asset/HowToUse/6_1_checkWork.jpg')} resizeMode='contain' />
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Text style={styles.h2} >６．スクリーンショット</ Text>
+                <Image style={{width: width,height:709*(width/500)}} source={require('../Asset/HowToUse/6_1_checkWork.jpg')} resizeMode='contain' />
                 <Text style={styles.h3} >スクリーンショットで作品を撮影して、SNSでシェアしてくださると嬉しいです。私も作品を見てみたいです。</ Text>
                 <Text style={styles.h3} >※SNSへの共有機能を実装予定です。</ Text>
-                <Text></Text>
-                <Text style={styles.h2}>７．セーブ</Text>
-                <Image style={styles.img1} source={require('../Asset/HowToUse/7_saveData.jpg')} resizeMode='contain' />
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Text style={styles.h2} >７．セーブ</ Text>
+                <Image style={{width: width,height:197*(width/500)}} source={require('../Asset/HowToUse/7_saveData.jpg')} resizeMode='contain' />
                 <Text style={styles.h3} >「セーブ」または「セーブ&クローズ」でデータを保存します</ Text>
                 <Text style={styles.h3} >作品を作りながらのこまめな「セーブ」をオススメします。</ Text>
-                <Text></Text>
+                <Text> </Text>
+                <Text> </Text>
+                <Text> </Text>
                 </ScrollView>
+
             </View>
+            <CloseButton />
         </View>
     )
 }
@@ -93,15 +121,19 @@ const styles = StyleSheet.create({
         opacity: 0.91,
     },
     mainLayout: {
-        top: '0%',
-        marginTop: '0%',
-        height: '100%',
+        height: '95%',
+        top: '5%',
         width: '100%',
+        paddingLeft: 0,
+        paddingRight:0,
+
     },
     h1: {
         fontSize: width/10,
-        color: 'darkblue',
-        backgroundColor: 'white'
+        color: 'white',
+        alignSelf: 'center',
+        justifyContent: 'center',
+
     },
     h2: {
         fontSize: width/15,
@@ -110,9 +142,18 @@ const styles = StyleSheet.create({
     h3: {
         fontSize: width/20,
         color: 'white',
+        alignSelf: 'center',
+        justifyContent: 'center',
     },
-    img1:{
-        width: '90%',
-        
+    closeBtn: {
+        position: 'absolute',
+        height: height * 0.06,
+        width: height * 0.06,
+        top: 10,
+        left: width - (height * 0.07),
+        backgroundColor: 'darkred',
+        borderRadius: 45,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
