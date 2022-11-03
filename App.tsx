de9151bf72263c8f439a1cedbe4f326a2553f94d
FrozenKota <q38230kota@gmail.com>
2022-11-03 17:02:04 +0900

    デバッグ環境構築

--- App.tsx
+++ App.tsx
@@ -2,9 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const message: Array<string> = [];
+
+  message.push('Hello ');
+  console.log(message);
+  message.push('World. '); 
+  console.log(message);
+  message.push('I am the Bug :->');
+  console.log(message);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{message.join('')}</Text>
       <StatusBar style="auto" />
     </View>
   );
