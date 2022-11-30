46149f1bcd7a4c6afa6ae0d601e305b9871b2348
AmaKota <kota@kota-ly.local>
2022-11-30 23:40:33 +0900

    ディレクトリ名変更 components -> src

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import StackNavigator from './components/Stack/StackNavigator';
+import StackNavigator from './src/Stack/StackNavigator';
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer >
       <StackNavigator />
-    </NavigationContainer>
+    </NavigationContainer >
   );
 }
 
