03f0e9db30936d5c5907c9577a6ccddb9b32a96a
FrozenKota <q38230kota@gmail.com>
2022-12-02 00:13:55 +0900

    SpeedDial 動作確認

--- src/BottomTab/EditorTab.tsx
+++ src/BottomTab/EditorTab.tsx
@@ -1,5 +1,8 @@
 import React, { ReactNode, useMemo } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import iSpeedDial from "../Common/iSpeedDial"
+import { SpeedDial } from '@rneui/themed';
+
 import {
     SafeAreaView,
     StyleSheet,
@@ -13,6 +16,7 @@ import {
 import Header from '../Common/Header';
 
 const EditorTab: React.FC = () => {
+    const [open, setOpen] = React.useState(false);
     const navigation = useNavigation();
     const right: ReactNode = useMemo(() => {
         return (
@@ -28,16 +32,24 @@ const EditorTab: React.FC = () => {
 
     return (
         <SafeAreaView style={styles.container}>
-            {false && (<Header title="編集画面" right={right} />)}
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignContent: 'center',
-                    backgroundColor: '#CCFFCC',
-                }}>
-                <Text style={{ textAlign: 'center' }}>編集画面</Text>
-            </View>
+            <SpeedDial
+                isOpen={open}
+                icon={{ name: 'edit', color: '#fff' }}
+                openIcon={{ name: 'close', color: '#fff' }}
+                onOpen={() => setOpen(!open)}
+                onClose={() => setOpen(!open)}
+            >
+                <SpeedDial.Action
+                    icon={{ name: 'add', color: '#fff' }}
+                    title="Add"
+                    onPress={() => console.log('Add Something')}
+                />
+                <SpeedDial.Action
+                    icon={{ name: 'delete', color: '#fff' }}
+                    title="Delete"
+                    onPress={() => console.log('Delete Something')}
+                />
+            </SpeedDial>
         </SafeAreaView>
     );
 }
@@ -45,7 +57,7 @@ const EditorTab: React.FC = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#00F",
+        backgroundColor: "fff",
         justifyContent: 'center',
         alignContent: 'center',
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
