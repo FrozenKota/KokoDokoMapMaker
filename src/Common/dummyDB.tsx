26e57f312754c55ca6326b878e9a5f67cc4b639f
FrozenKota <q38230kota@gmail.com>
2023-02-25 13:58:00 +0900

    modified:   dummyDB.tsx 　セーブデータのプロパティ修正

--- src/Common/dummyDB.tsx
+++ src/Common/dummyDB.tsx
@@ -1,38 +1,40 @@
 type saveDataType = {
-    fileName: string,
     key: string,
+    fileName: string,
     initStatus: {
-        location: boolean,
-        divNum: boolean,
+        isInitializedLocation: boolean,
+        isInitializedUnit: boolean,
     },
-    divNumX: number,
-    divNumY: number,
+    unitLatitude: number,
+    unitLongitude: number,
     region: {
         latitude: number,
         longitude: number,
         latitudeDelta: number,
         longitudeDelta: number,
     },
-    imgData: {
+    mapData: {
         [key: string]: {
-            PosX: number,
-            PosY: number,
+            imgType: string,
+            imgName: string,
+            posX: number,
+            posY: number,
             layer: number,
-            sizeRatio: number,
-            imgName: String
-        }
-    }
-};
+            imgRatio: number,
+            imgRotate: number,
+        },
+    },
+}
 
 export const dummyDB: saveDataType = {
     fileName: "",
-    key: '202212031909-01',
+    key: '20221203190900', // YYYYMMDDhhmmss
     initStatus: {
-        location: true,
-        divNum: true,
+        isInitializedLocation: true,
+        isInitializedUnit: true,
     },
-    divNumX: 10,
-    divNumY: 10,
+    unitLatitude: 10,
+    unitLongitude: 10,
     region: {
         latitude: 38.165510778804716,
         //latitude: 37.8025259,
@@ -41,7 +43,15 @@ export const dummyDB: saveDataType = {
         latitudeDelta: 0.5,
         longitudeDelta: 10,
     },
-    imgData: {
-        xy01: { PosX: 0, PosY: 1, layer: 2, sizeRatio: 2, imgName: 'Asset0_1', },
+    mapData: {
+        xy01: {
+            imgType: "img",
+            imgName: 'Asset0_1',
+            posX: 0,
+            posY: 1,
+            layer: 2,
+            imgRatio: 2,
+            imgRotate: 0,
+        },
     },
 }
