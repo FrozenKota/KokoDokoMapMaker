d2bd1122ee1c4783d81f90957ffb5dd4ad704b88
FrozenKota <q38230kota@gmail.com>
2023-02-25 21:50:09 +0900

    modified:   .expo/packager-info.json         modified:   src/BottomTab/EditorTab.tsx         modified:   src/Common/dummyDB.tsx dummyDBのセーブデータから表示できるように修正
    バグ:
    　画面外に画像が移動すると、内側に入り込む方向に位置ズレ発生してめちゃくちゃになる

--- src/BottomTab/EditorTab.tsx
+++ src/BottomTab/EditorTab.tsx
@@ -19,7 +19,7 @@ import GridLine from '../Editor/GridLine';
 import images from '../../Asset/asset';
 
 const { width, height } = Dimensions.get('window');
-const STATUSBAR_HEIGHT = (Platform.OS === "android" && StatusBar.currentHeight != undefined) ? StatusBar.currentHeight : 0;
+const STATUSBAR_HEIGHT = (Platform.OS === "android" && StatusBar.currentHeight !== undefined) ? StatusBar.currentHeight : 0;
 const HEIGHT = height - STATUSBAR_HEIGHT;
 
 const EditorTab: React.FC = () => {
@@ -38,12 +38,14 @@ const EditorTab: React.FC = () => {
         West_log: 0,
         South_lat: 0
     })
-    const [px1, setPx1] = React.useState(0);
-    const [py1, setPy1] = React.useState(0);
-    const [px2, setPx2] = React.useState(0);
-    const [py2, setPy2] = React.useState(0);
+    // const [px1, setPx1] = React.useState(0);
+    // const [py1, setPy1] = React.useState(0);
+    // const [px2, setPx2] = React.useState(0);
+    // const [py2, setPy2] = React.useState(0);
 
     const items = [];
+    const items_img: any = [];
+
     const navigation = useNavigation();
 
     const Actions = [
@@ -62,6 +64,13 @@ const EditorTab: React.FC = () => {
         });
     }
     const onRegionChange = (e: any) => {
+        setRegion({
+            latitude: e.latitude,
+            latitudeDelta: e.latitudeDelta,
+            longitude: e.longitude,
+            longitudeDelta: e.longitudeDelta
+        })
+
         const cornerRegionTemp = {
             North_lat: e.latitude + e.latitudeDelta / 2,
             East_log: e.longitude + e.longitudeDelta / 2,
@@ -69,141 +78,37 @@ const EditorTab: React.FC = () => {
             South_lat: e.latitude - e.latitudeDelta / 2,
         }
         setCornerRegion(cornerRegionTemp);
-
-        setRegion({
-            latitude: e.latitude,
-            latitudeDelta: e.latitudeDelta,
-            longitude: e.longitude,
-            longitudeDelta: e.longitudeDelta
-        })
-        const imgSize = 0.05; // latitude 10deg, longitude 10deg
-        const imgLongitude = 139.6191623993218;
-        const imgLatitude = 35.13401895914322;
-
-        setPx1(viewSize.width * Math.abs(imgLongitude - cornerRegionTemp.West_log) / e.longitudeDelta);
-        setPy1(viewSize.height * Math.abs(imgLatitude - cornerRegionTemp.North_lat) / e.latitudeDelta);
-        setPx2(viewSize.width * (Math.abs(imgLongitude - cornerRegionTemp.West_log) + imgSize) / e.longitudeDelta);
-        setPy2(viewSize.height * (Math.abs(imgLatitude - cornerRegionTemp.North_lat) + imgSize) / e.latitudeDelta);
     }
 
-    let i;
-    i = 100;
-    items.push(
-        <Polyline
-            key={i += 1}
-            coordinates={[
-                { latitude: 0, longitude: 179.99 },
-                { latitude: 0, longitude: 0 },
-                { latitude: 0, longitude: -179.99 }
-            ]}
-            strokeColor="#F00" // fallback for when `strokeColors` is not supported by the map-provider
-            strokeColors={[
-                '#000000',
-                '#000000',
-                '#000000',
-            ]}
-            strokeWidth={3}
-        />
-    )
-    items.push(
-        <Polyline
-            key={i += 1}
-            coordinates={[
-                { latitude: 89.99, longitude: 0 },
-                { latitude: 0, longitude: 0 },
-                { latitude: -89.99, longitude: 0 }
-            ]}
-            strokeColor="#F00" // fallback for when `strokeColors` is not supported by the map-provider
-            strokeColors={[
-                '#000000',
-                '#000000',
-                '#000000',
-            ]}
-            strokeWidth={3}
-        />
-    )
-    items.push(
-        <Polyline
-            key={i += 1}
-            coordinates={[
-                { latitude: 89.99, longitude: 90 },
-                { latitude: 0, longitude: 90 },
-                { latitude: -89.99, longitude: 90 }
-            ]}
-            strokeColor="#F00" // fallback for when `strokeColors` is not supported by the map-provider
-            strokeColors={[
-                '#000000',
-                '#000000',
-                '#000000',
-            ]}
-            strokeWidth={3}
-        />
-    )
-    items.push(
-        <Polyline
-            key={i += 1}
-            coordinates={[
-                { latitude: 89.99, longitude: -90 },
-                { latitude: 0, longitude: -90 },
-                { latitude: -89.99, longitude: -90 }
-            ]}
-            strokeColor="#F00" // fallback for when `strokeColors` is not supported by the map-provider
-            strokeColors={[
-                '#000000',
-                '#000000',
-                '#000000',
-            ]}
-            strokeWidth={3}
-        />
-    )
+    const mapDataKeys = Object.keys(dummyDB.mapData);
 
-    items.push(
-        <Polyline
-            key={i += 1}
-            coordinates={[
-                { latitude: 89.99, longitude: 180 },
-                { latitude: 0, longitude: 180 },
-                { latitude: -89.99, longitude: 180 }
-            ]}
-            strokeColor="#F00" // fallback for when `strokeColors` is not supported by the map-provider
-            strokeColors={[
-                '#000000',
-                '#000000',
-                '#000000',
-            ]}
-            strokeWidth={3}
-        />
-    )
-    items.push(
-        <Polyline
-            key={i += 1}
-            coordinates={[
-                { latitude: cornerRegion.North_lat, longitude: cornerRegion.West_log },
-                { latitude: cornerRegion.South_lat, longitude: cornerRegion.East_log },
-            ]}
-            strokeColor="#FF0" // fallback for when `strokeColors` is not supported by the map-provider
-            strokeColors={[
-                '#000000',
-                '#000000',
-            ]}
-            strokeWidth={2}
-        />
-    )
-    items.push(
-        <Polyline
-            key={i += 1}
-            coordinates={[
-                { latitude: cornerRegion.North_lat, longitude: cornerRegion.East_log },
-                { latitude: cornerRegion.South_lat, longitude: cornerRegion.West_log },
-            ]}
-            strokeColor="#FF0" // fallback for when `strokeColors` is not supported by the map-provider
-            strokeColors={[
-                '#000000',
-                '#000000',
-            ]}
-            strokeWidth={2}
-        />
-    )
+    mapDataKeys.map((value, index, array) => {
+        let imgLongitude = dummyDB.mapData[value].longitude;
+        let imgLatitude = dummyDB.mapData[value].latitude;
+        let imgRatio = dummyDB.mapData[value].imgRatio;
+
+        let px1 = viewSize.width * Math.abs(imgLongitude - cornerRegion.West_log) / region.longitudeDelta;
+        let py1 = viewSize.height * Math.abs(imgLatitude - cornerRegion.North_lat) / region.latitudeDelta;
+        let px2 = viewSize.width * (Math.abs(imgLongitude - cornerRegion.West_log) + imgRatio) / region.longitudeDelta;
+        let py2 = viewSize.height * (Math.abs(imgLatitude - cornerRegion.North_lat) + imgRatio) / region.latitudeDelta;
+
+        //if (dummyDB.mapData[value].longitude <= cornerRegion.East_log) {
+        items_img.push(
+            <Image
+                key={value}
+                style={{
+                    position: 'absolute',
+                    top: STATUSBAR_HEIGHT + py1,
+                    left: px1,
+                    width: px2 - px1,
+                    height: py2 - py1
+                }}
+                resizeMode='contain'
+                source={images['yellow_tail_fish']}
+            />
+        )
+        //}
+    })
 
     return (
         <SafeAreaView style={styles.container} onLayout={(e) => { onLayout(e) }}>
@@ -215,10 +120,8 @@ const EditorTab: React.FC = () => {
                 zoomEnabled={true}
                 onRegionChangeComplete={(e) => { onRegionChange(e) }}
             >
-                {items}
             </MapView>
-            <Image style={{ position: 'absolute', top: StatusBar.currentHeight + py1, left: px1, width: px2 - px1, height: py2 - py1 }} resizeMode='contain' source={images['white_solt_water3']} />
-
+            {items_img}
             <SpeedDial
                 isOpen={open}
                 size='large'
